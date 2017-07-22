0c9dd04054742c1a47974e0d54b4c8f69423546d
kapucko <dkapisinsky@gmail.com>
2017-07-22 18:19:16 +0200

    web UI

--- btsearch/app/app.py
+++ btsearch/app/app.py
@@ -1,18 +1,23 @@
 from datetime import datetime
 
-from flask import Flask, jsonify, request
+from flask import Flask,render_template, request
 
 from btsearch.searchers import RegioJetSearcher
 
 app = Flask(__name__)
 app.config['PROPAGATE_EXCEPTIONS'] = True
 
-
-@app.route('/api/search', methods=['GET'])
+@app.route("/search")
 def search():
+   return render_template('search_form.html', my_string='Hello world')
+
+@app.route("/results")
+def get_routes():
+    print(request.args)
     searcher = RegioJetSearcher()
     d = datetime.strptime(request.args.get('date'), '%Y-%m-%d')
     from_ = request.args.get('from')
     to = request.args.get('to')
     results = searcher.get_route(from_, to, d)
-    return jsonify(results)
+
+    return render_template('results.html', connections=results)
